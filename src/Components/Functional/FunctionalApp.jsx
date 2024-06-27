e50e856d9bd31771ee8a4f71b96630c0caded049
LaToya Simon <1stforgottentoy@gmail.com>
2024-06-27 17:07:47 -0400

    added github link and tracking user input

--- src/Components/Functional/FunctionalApp.jsx
+++ src/Components/Functional/FunctionalApp.jsx
@@ -1,12 +1,17 @@
 import { FunctionalGameBoard } from "./FunctionalGameBoard";
 import { FunctionalScoreBoard } from "./FunctionalScoreBoard";
 import { FunctionalFinalScore } from "./FunctionalFinalScore";
+// import { useState } from "react";
 
 export function FunctionalApp() {
+  //   const [userInput, setUserInput] = useState("");
+
   return (
     <>
       <FunctionalScoreBoard />
       <FunctionalGameBoard />
+      {/* <FunctionalScoreBoard userInput={userInput} />
+      <FunctionalGameBoard userInput={userInput} /> */}
       {false && <FunctionalFinalScore />}
     </>
   );
--- src/Components/Functional/FunctionalGameBoard.jsx
+++ src/Components/Functional/FunctionalGameBoard.jsx
@@ -1,5 +1,6 @@
 import "./styles/game-board.css";
 import { Images } from "../../assets/Images";
+import { useState } from "react";
 
 const initialFishes = [
   {
@@ -20,16 +21,45 @@ const initialFishes = [
   },
 ];
 
+//onSubmit and state here for userInput
+//track correct and incorrect answers
+//if userInput matches nextFishToName.name, add 1 to correct
+//if it doesn't, add 1 to incorrect
+//move to the next fish to name in the list
+//when moving to the next fish, regardless of if answer is right or not, the correct answer for that fish is removed from the list of answers
+
 export function FunctionalGameBoard() {
   const nextFishToName = initialFishes[0];
+
+  const [userInput, setUserInput] = useState("");
+  console.log("this is userInput:", userInput);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log("this is userInput: ", userInput);
+    console.log("this is nextFishToName: ", nextFishToName);
+    // userInput === nextFishToName.name
+    //   ? (correctCount = correctCount + 1)
+    //   : (incorrectCount = incorrectCount + 1);
+
+    setUserInput("");
+  };
   return (
     <div id="game-board">
       <div id="fish-container">
         <img src={nextFishToName.url} alt={nextFishToName.name} />
       </div>
-      <form id="fish-guess-form">
+      <form id="fish-guess-form" onSubmit={onSubmit}>
         <label htmlFor="fish-guess">What kind of fish is this?</label>
-        <input type="text" name="fish-guess" />
+        <input
+          type="text"
+          name="fish-guess"
+          value={userInput}
+          onChange={(e) => {
+            setUserInput(e.target.value);
+            // console.log("this is userInput:", userInput);
+          }}
+        />
         <input type="submit" />
       </form>
     </div>
